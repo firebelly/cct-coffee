d1357e650def397ccebb5610dc1b0492a85d5ea3
Nate Beaty <nate@firebellydesign.com>
2012-12-05 13:21:40 -0600

    typo

--- open_displays_coffee_room.ps1
+++ open_displays_coffee_room.ps1
@@ -15,4 +15,4 @@ function Run_IE ([int]$resX, [int]$resY, [int]$shiftX, [string]$site_addr) {
 $ie1 = Run_IE 1920 1080 0 "C:\inetpub\wwwroot\cct-coffee\4goals.html"
 $ie2 = Run_IE 1920 1080 1920 "C:\inetpub\wwwroot\cct-coffee\5values.html"
 $ie3 = Run_IE 1920 1080 3840 "C:\inetpub\wwwroot\cct-coffee\slide.html"
-$ie4 = Run_IE 1920 1080 -192040 "C:\inetpub\wwwroot\cct-coffee\definition.html"
+$ie4 = Run_IE 1920 1080 -1920 "C:\inetpub\wwwroot\cct-coffee\definition.html"
